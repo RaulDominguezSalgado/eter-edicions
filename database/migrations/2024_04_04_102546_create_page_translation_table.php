a5289908057bd894ab518fef9ebf10252c9d5a0b
freddy32891 <freddyachata@gmail.com>
2024-04-04 12:48:38 +0200

    Adding pages table in the migration and updating existing migrations

---
+++ database/migrations/2024_04_04_102546_create_page_translation_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('page_translation', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('page_id');
+            $table->string('lang');
+            $table->string('name');
+            $table->text('description');
+            $table->string('slug');
+            $table->timestamps();
+
+            $table->foreign('page_id')->references('id')->on('pages')->name('pages_translation_page_id_foreign');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('page_translation');
+    }
+};
