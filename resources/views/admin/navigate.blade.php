e27926d09f414877a0d0341b6bf3f7db9177ce53
freddy32891 <freddyachata@gmail.com>
2024-04-05 14:29:51 +0200

    Adding admin menu

---
+++ resources/views/admin/navigate.blade.php
@@ -0,0 +1,19 @@
+<nav>
+    <ul>
+        <li><a href="">Logo</a></li>
+    </ul>
+    <ul>
+        <li><a href="">Portada</a></li>
+        <li><a href="">Catàleg</a></li>
+        <li><a href="{{ route('collaborators.index') }}">Colaborators</a></li>
+        <li><a href="">Agència</a></li>
+        <li><a href="">Activitats</a></li>
+        <li><a href="">Articles</a></li>
+        <li><a href="">Qui som</a></li>
+
+    </ul>
+    <ul>
+        <li><button>Buscar</button></li>
+    </ul>
+</nav>
+<hr>
