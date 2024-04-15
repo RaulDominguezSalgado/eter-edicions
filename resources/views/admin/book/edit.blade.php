eede9a6dfba0b4aae20cf5d8e510ed9bbbe7eb1c
ruyouhuye <ruyouhuye@gmail.com>
2024-04-15 13:47:08 +0200

    modifying book edit blade

--- resources/views/admin/book/edit.blade.php
+++ resources/views/admin/book/edit.blade.php
@@ -7,7 +7,7 @@
                         <div style="display: flex; justify-content: space-between; align-items: center;">
                             <span id="card_title">
                                 <a href="{{ route('books.index') }}">Torna a l'index</a>
-                                <h1>Editant "{{ $book['title']}}"</h1>
+                                <h1>Editant "{{ $book['title'] }}"</h1>
                             </span>
                         </div>
                     </div>
@@ -23,7 +23,8 @@
                             <div class="mr-6 cover">
                                 {{-- <div id="book-image" class="book-image"></div> --}}
                                 {{-- <div class="object-fill cover border-guide-2"> --}}
-                                <img class="" src="{{ asset('img/books/covers/' . $book['image']) }}" alt="{{ $book['title'] }}">
+                                <img class="" src="{{ asset('img/books/covers/' . $book['image']) }}"
+                                    alt="{{ $book['title'] }}">
                                 {{-- </div> --}}
                             </div>
 
@@ -32,17 +33,36 @@
                                 <div class="flex flex-col space-y-3">
                                     <div class="title-author flex flex-col space-y-">
                                         <h2>{{ $book['title'] }}</h2>
+                                        <h3>Dades del llibre</h3>
+                                        <h5>Títol: {{ $book['title'] }}</h5>
                                         <div class="flex space-x-1">
                                             @foreach ($book['authors'] as $author)
                                                 {{-- if not last iteration --}}
+
                                                 @if (!$loop->last)
-                                                    <h5>{{ $author }},</h5>
+                                                    <h5>Autoria: {{ $author }},</h5>
                                                     {{-- if last iteration --}}
                                                 @else
-                                                    <h5>{{ $author }}</h5>
+                                                    <h5> Autoria: {{ $author }}</h5>
                                                 @endif
                                             @endforeach
                                         </div>
+                                        Traducció:
+                                        @foreach ($book['translators'] as $translator)
+                                            {{-- if not last iteration --}}
+
+                                            @if (!$loop->last)
+                                                <h5> {{ $translator }},</h5>
+                                                {{-- if last iteration --}}
+                                            @else
+                                                <h5>{{ $translator }}</h5>
+                                            @endif
+                                        @endforeach
+                                    </div>
+
+                                    <div id="book-description">
+                                        <h5>Descripció:</h5>
+                                        <p>{{ $book['description'] }}</p>
                                     </div>
 
                                     <div class="flex space-x-1.5" id="pvp">
@@ -54,13 +74,10 @@
                                         @endif
                                     </div>
 
-                                    <div id="book-description">
-                                        <p>{{ $book['description'] }}</p>
-                                    </div>
                                 </div>
 
                                 <div class="flex flex-col space-y-2.5">
-
+                                    
 
 
                                 </div>
