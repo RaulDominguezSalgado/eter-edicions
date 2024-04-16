7710466841e67ed65870fc72f7a730efd02ccd46
Jordi Rissech <jordi.rissech.gasulla@gmail.com>
2024-04-16 14:45:09 +0200

    - Book blade in process

--- resources/views/public/book.blade.php
+++ resources/views/public/book.blade.php
@@ -6,8 +6,8 @@
 
     <link rel="stylesheet" href="{{ asset('css/public/book.css') }}">
 
-    <div class="body space-y-24 mb-12">
-        <div class="book flex flex-col min-h-screen">
+    <div class="body space-y-32 mb-12">
+        <div class="book mt-16">
             <div class="book-detail flex justify-between mb-4">
                 <div class="mr-6 cover">
                     {{-- <div id="book-image" class="book-image"></div> --}}
