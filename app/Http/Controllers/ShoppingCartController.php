d0067e7eefc279dd8276fc40f8e48eb370c685a7
freddy32891 <freddyachata@gmail.com>
2024-04-26 14:44:54 +0200

    Saving changes before merge

---
+++ app/Http/Controllers/ShoppingCartController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+/**
+ * Class ShoppingCartController
+ * @package App\Http\Controllers
+ */
+class ShoppingCartController extends Controller
+{
+    function addProduct($product){
+        
+    }
+}
