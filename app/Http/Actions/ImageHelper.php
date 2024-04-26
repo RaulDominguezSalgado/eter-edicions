a1939c24e42b64802fadf6365e3ed249c2370150
freddy32891 <freddyachata@gmail.com>
2024-04-26 12:19:21 +0200

    Añadimos el ImageHelper

--- app/Http/Actions/ImageHelper.php
+++ app/Http/Actions/ImageHelper.php
@@ -12,67 +12,87 @@ use Intervention\Image\Encoders\WebpEncoder;
  */
 class ImageHelper
 {
-    private $paths = [
+    public static $paths = [
         'books' => [
-            'thumbnail' => 'img/books/thumbnails/',
-            'portada' => 'img/books/portadas/',
+            'thumbnail' => '/img/books/thumbnails/',
+            'portada' => '/img/books/portadas/',
         ],
         'collaborators' => [
-            'thumbnail' => '/collaborators/thumbnails/',
-            'portada' => '/collaborators/portada/',
+            'thumbnail' => '/img/collaborators/thumbnails/',
+            'portada' => '/img/collaborators/portada/',
         ],
     ];
 
-    private $sizes = [
-        'books' => [
-            'reduction_rate' => .6,
-            'original' => ['', '']
-        ],
-        'collaborators' => [
-            'reduction_rate' => .6,
-            'original' => ['', '']
-        ],
+    public static $sizes = [
+        'books' => ['720', '1080'],
+        'collaborators' => ['560', '400'],
     ];
 
+    public static $reduction_rate = .6;
 
-    public function editImage($fileName, $typeImage)
+
+
+
+    private static function configImage($path, $size)
     {
-        $reductionRate = 0.6;
-
-
-        switch ($typeImage) {
-            case "collaborator":
-
-                ImageHelper::configImage($this->paths['books']['portada'] . $fileName,0,0); //todo
-                // ImageHelper::configImage($this->paths['books']['thumbnail'].$fileName);
-                $rutaImagen = public_path('img/collab/covers/');
-                $rutaThumbnail = public_path('img/collab/thumbnails/');
-
-                //intval($new_width * (1 / 1.5));
-
-                break;
-            case "book":
-                break;
-            default:
-                break;
-        }
-    }
-    private static function configImage($path, $width, $height)
-    {
+        $width = $size[0];
+        $height = $size[1];
         $manager = new ImageManager(new Driver());
         $image = $manager->read($path);
         $image_width = $image->width();
         $image_height = $image->height();
-
-        $new_width = 668;
-        $new_height = 446;
-        if ($image_width > $new_width || $image_height > $new_height) {
-            $image->cover($new_width, $new_height, "center");
+        if ($image_width > $width || $image_height > $height) {
+            $image->cover($width, $height, "center");
         }
         // Encode the image to webp format with 80% quality
         $image->encode(new WebpEncoder(), 80);
 
         // Save the processed image
         $image->save($path);
+    }
+
+
+
+
+    public static function editImage($fileName, $typeImage)
+    {
+        switch ($typeImage) {
+            case "collaborator":
+                $rutaImagen = self::$paths['collaborators']['portada'];
+                $rutaThumbnail = self::$paths['collaborators']['thumbnail'];
+                $tamaño_original = self::$sizes['collaborators'];
+                $tamaño_thumbnail = [
+                    self::$sizes['collaborators'][0]*self::$reduction_rate,
+                    self::$sizes['collaborators'][1]*self::$reduction_rate
+                ];
+            break;
+            case "book":
+                $rutaImagen = self::$paths['books']['portada'];
+                $rutaThumbnail = self::$paths['books']['thumbnail'];
+                $tamaño_original = self::$sizes['books'];
+                $tamaño_thumbnail = [
+                    self::$sizes['books'][0]*ImageHelper::$reduction_rate,
+                    self::$sizes['books'][1]*self::$reduction_rate
+                ];
+            break;
+            default:
+            break;
+        }
+
+        self::configImage($rutaImagen.$fileName,$tamaño_original);
+        self::configImage($rutaThumbnail.$fileName,$tamaño_thumbnail);
+    }
+
+    public static function regenerateImages() {
+        foreach (self::$paths as $post_type) {
+            foreach ($post_type as $path) {
+                $ficheros = scandir($_SERVER['DOCUMENT_ROOT'].$path);
+                foreach ($ficheros as $fichero) {
+                    if ($fichero != '.' && $fichero != '..' && strpos($fichero, '.') !== 0) {
+                        self::editImage($fichero, $path);
+                    }
+                }
+            }
+        }
     }
 }
