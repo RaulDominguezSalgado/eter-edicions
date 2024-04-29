58a2d2ed7c9a395f65583d7a71a7e3e42085047e
freddy32891 <freddyachata@gmail.com>
2024-04-29 11:29:31 +0200

    Fixing Image Helper errors

--- app/Http/Actions/imageHelper.php
+++ app/Http/Actions/imageHelper.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Actions;
 
+use Illuminate\Support\Facades\File;
 use Intervention\Image\ImageManager;
 use Intervention\Image\Drivers\Gd\Driver;
 use Intervention\Image\Encoders\WebpEncoder;
@@ -12,67 +13,96 @@ use Intervention\Image\Encoders\WebpEncoder;
  */
 class ImageHelper
 {
-    private $paths = [
+    public static $paths = [
         'books' => [
             'thumbnail' => 'img/books/thumbnails/',
-            'portada' => 'img/books/portadas/',
+            'cover' => 'img/books/covers/',
         ],
         'collaborators' => [
-            'thumbnail' => '/collaborators/thumbnails/',
-            'portada' => '/collaborators/portada/',
+            'thumbnail' => 'img/collab/thumbnails/',
+            'cover' => 'img/collab/covers/',
         ],
     ];
 
-    private $sizes = [
-        'books' => [
-            'reduction_rate' => .6,
-            'original' => ['', '']
-        ],
-        'collaborators' => [
-            'reduction_rate' => .6,
-            'original' => ['', '']
-        ],
+    public static $sizes = [
+        'books' => ['720', '1080'],
+        'collaborators' => ['560', '400'],
     ];
 
+    public static $reduction_rate = .6;
 
-    public function editImage($fileName, $typeImage)
+
+
+
+    private static function configImage($readPath, $savePath, $size)
     {
-        $reductionRate = 0.6;
+        // dump($readPath);
+        // dump($savePath);
+        // dump($size);
 
-
-        switch ($typeImage) {
-            case "collaborator":
-
-                ImageHelper::configImage($this->paths['books']['portada'] . $fileName,0,0); //todo
-                // ImageHelper::configImage($this->paths['books']['thumbnail'].$fileName);
-                $rutaImagen = public_path('img/collab/covers/');
-                $rutaThumbnail = public_path('img/collab/thumbnails/');
-
-                //intval($new_width * (1 / 1.5));
-
-                break;
-            case "book":
-                break;
-            default:
-                break;
-        }
-    }
-    private static function configImage($path, $width, $height)
-    {
+        $width = $size[0];
+        $height = $size[1];
         $manager = new ImageManager(new Driver());
-        $image = $manager->read($path);
+
+        // dd($manager->read($readPath));
+
+        $image = $manager->read(public_path($readPath));
         $image_width = $image->width();
         $image_height = $image->height();
-
-        $new_width = 668;
-        $new_height = 446;
-        if ($image_width > $new_width || $image_height > $new_height) {
-            $image->cover($new_width, $new_height, "center");
+        if ($image_width > $width || $image_height > $height) {
+            $image->cover($width, $height, "center");
         }
         // Encode the image to webp format with 80% quality
-        $image->encode(new WebpEncoder(), 80);
+        // dd($image->encode(new WebpEncoder(), 80));
 
         // Save the processed image
-        $image->save($path);
+        $image->save(public_path($savePath));
+    }
+
+
+
+
+    public static function editImage($fileName, $typeImage)
+    {
+        switch ($typeImage) {
+            case "collaborator":
+                $rutaImagen = self::$paths['collaborators']['cover'];
+                $rutaThumbnail = self::$paths['collaborators']['thumbnail'];
+                $tamaño_original = self::$sizes['collaborators'];
+                $tamaño_thumbnail = [
+                    self::$sizes['collaborators'][0]*self::$reduction_rate,
+                    self::$sizes['collaborators'][1]*self::$reduction_rate
+                ];
+            break;
+            case "book":
+                $rutaImagen = self::$paths['books']['cover'];
+                $rutaThumbnail = self::$paths['books']['thumbnail'];
+                $tamaño_original = self::$sizes['books'];
+                $tamaño_thumbnail = [
+                    self::$sizes['books'][0]*ImageHelper::$reduction_rate,
+                    self::$sizes['books'][1]*self::$reduction_rate
+                ];
+            break;
+            default:
+            break;
+        }
+
+        self::configImage('/img/temp/'.$fileName, $rutaImagen.$fileName,$tamaño_original);
+        self::configImage('/img/temp/'.$fileName, $rutaThumbnail.$fileName,$tamaño_thumbnail);
+
+        File::deleteDirectory(public_path('/img/temp/'));
+    }
+
+    public static function regenerateImages() {
+        foreach (self::$paths as $post_type) {
+            foreach ($post_type as $path) {
+                $ficheros = scandir($_SERVER['DOCUMENT_ROOT'].$path);
+                foreach ($ficheros as $fichero) {
+                    if ($fichero != '.' && $fichero != '..' && strpos($fichero, '.') !== 0) {
+                        self::editImage($fichero, $path);
+                    }
+                }
+            }
+        }
     }
 }
