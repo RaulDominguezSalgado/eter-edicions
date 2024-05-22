af4886c5232c0cb72553a11f8db7710a4751466f
Raul <rauldominguezsalgado@gmail.com>
2024-05-22 14:04:50 +0200

    Añadido banner de cookies

---
+++ resources/views/components/partials/cookieBanner.blade.php
@@ -0,0 +1,22 @@
+<div class="bg-black bg-opacity-60 fixed top-0 left-0 right-0 bottom-0 inset-x-0 flex content-center items-center z-50">
+    <div class="max-w-7xl mx-auto bg-white rounded-lg">
+        <header class="bg-black text-white p-3">
+            <h2 class="font-light normal-case">La nostra página utilitza cookies</h2>
+        </header>
+        <main>
+            <p>A Eter Edicions utilitzem cookies per millorar l'experiencia. Pots acceptarles, denegrles o configurarles.</p>
+        </main>
+        <footer class="flex justify-end p-3">
+            <button>Acceptar</button>
+            <button>Denegar</button>
+            <button>Configurar</button>
+        </footer>
+    </div>
+</div>
+<style>
+    button {
+        border: 1px solid black;
+        margin: 0 10px;
+        padding: 5px 10px;
+    }
+</style>
