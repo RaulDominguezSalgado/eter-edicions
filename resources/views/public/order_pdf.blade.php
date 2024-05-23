b6536ccadad9d8f5de72b7a535d1cbec1002dfe6
Jordi Rissech <jordi.rissech.gasulla@gmail.com>
2024-05-23 15:02:51 +0200

    - Checkout responsive - purchaseCompleted ticket almost done

--- resources/views/public/order_pdf.blade.php
+++ resources/views/public/order_pdf.blade.php
@@ -24,7 +24,7 @@ $base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
 
 <body>
 
-    <div class="w-min flex flex-col justify-center">
+    {{-- <div class="w-min flex flex-col justify-center">
         <div class="ticket space-y-2">
             <div class="items-baseline pb-1">
                 <h2 class="float-left">{{ __('checkout.ticket') }}</h2>
@@ -34,12 +34,9 @@ $base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
             <hr>
             <div class="order-details">
                 <p>{{ __('form.name') }}: {{ $order->first_name . ' ' . $order->last_name }}</p>
-                <p>{{ __('form.shipping-address') }}: {{ $order->address . ', ' . $order->zip_code . ', ' . $order->city . '.' }}</p>
+                <p>{{ __('form.shipping-address') }}: {{ $order->address . ' ' . $order->apartment . ', ' . $order->zip_code . ', ' . $order->locality . '.' }}</p>
                 <p>{{ __('form.date') }}: {{ $order->date }}</p>
                 <p>{{ __('form.reference-number') }}: {{ $order->reference }}</p>
-                <p>{{ __('form.payment-method') }}: {{ $paymentMethod }}</p>
-                <p>{{ __('form.shipping-cost') }}: {{ $order->shipment_taxes }}€</p>
-                <p>{{ __('shopping-cart.total') }}: {{ $order->total }}€</p>
                 <!-- Other order details -->
             </div>
 
@@ -63,12 +60,67 @@ $base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
                     @endforeach
                 </tbody>
             </table>
+
+            <div>
+                <p>{{ __('form.payment-method') }}: {{ $paymentMethod }}</p>
+                <p>{{ __('form.shipping-cost') }}: {{ $order->shipment_taxes }}€</p>
+                <p>{{ __('shopping-cart.total') }}: {{ $order->total }}€</p>
+            </div>
+        </div>
+        <div class="footer">
+            {{ __('checkout.thanks-for-your-purchase') }}
+        </div>
+    </div> --}}
+
+
+    <div class="w-max flex flex-col justify-center space-y-4">
+        <div class="ticket md:w-[500px] bg-[#f9f9f9] p-5 space-y-4">
+            <div class="items-baseline pb-1">
+                <h2 class="float-left">{{ __('checkout.ticket') }}</h2>
+                <img src="{{ $base64 }}" alt="Èter Edicions" style="width: 3em; height: 3em" class="float-right">
+            </div>
+            <br class="clear-both">
+            <hr>
+            <div class="order-details">
+                <p><strong>{{ __('form.name') }}:</strong></p><p class="mb-1"> {{ $order->first_name . ' ' . $order->last_name }}</p>
+                <p><strong>{{ __('form.shipping-address') }}:</strong></p><p class="mb-1">{{ $order->address }}, @if($order->apartment) {{$order->apartment}},@endif <br>{{$order->locality}}, {{$order->zip_code}}</p>
+                <p><strong>{{ __('form.date') }}:</strong></p><p class="mb-1"> {{ $order->date }}</p>
+                <p><strong>{{ __('form.reference-number') }}:</strong></p><p class="mb-1"> {{ $order->reference }}</p>
+                <p><strong>{{ __('form.payment-method') }}:</strong></p><p class="mb-1"> {{ $order->payment_method }}</p>
+                <!-- Other order details -->
+            </div>
+
+            <table class="order-items">
+                <thead>
+                    <tr>
+                        <th class="text-start md:pe-2">{{ __('shopping-cart.product') }}</th>
+                        <th class="text-end px-1 md:px-2">#</th>
+                        <th class="text-end px-3 md:px-6">{{ __('shopping-cart.price') }}</th>
+                        <th class="text-end px-3 md:ps-6">{{ __('shopping-cart.total') }}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($order->details()->get() as $detail)
+                        <tr>
+                            <td class="text-start md:pe-2">{{ $detail->book->title }}</td>
+                            <td class="text-end px-1 md:px-2">{{ $detail->quantity }}</td>
+                            <td class="text-end px-3 md:px-6">{{ $detail->price_each }}€</td>
+                            <td class="text-end px-3 md:ps-6"><strong>{{ number_format($detail->quantity * $detail->price_each, 2) }}€</strong></td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+
+            <div>
+                <p><strong>{{ __('shopping-cart.subtotal') }}:</strong></p><p class="mb-1"> {{ number_format($order->total - $order->shipment_taxes) }}€</p>
+                <p><strong>{{ __('form.shipping-cost') }}:</strong></p><p class="mb-1"> {{ $order->shipment_taxes }}€</p>
+                <p><strong>{{ __('shopping-cart.total') }}:</strong></p><p class="mb-1"> {{ $order->total }}€</p>
+            </div>
         </div>
         <div class="footer">
             {{ __('checkout.thanks-for-your-purchase') }}
         </div>
     </div>
-
 
 
 </body>
--- resources/views/public/purchaseCompleted.blade.php
+++ resources/views/public/purchaseCompleted.blade.php
@@ -1,22 +1,76 @@
 <?php
 $locale = 'ca';
+
+$path = 'img/logo/lg/logo_eter_black.webp';
+$type = pathinfo($path, PATHINFO_EXTENSION);
+$data = file_get_contents($path);
+$base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
+
 ?>
 <x-layouts.app>
     {{-- <h1>Checkout</h1> --}}
     <div class="">
-        <div class="w-2/3 float-left pr-3">
+        <div class="w-full lg:w-2/3 float-left px-3">
             <div class="space-y-4">
-                <h2>{{__('checkout.purchase-completed')}}</h2>
+                <h2 class="">{{__('checkout.purchase-completed')}}</h2>
                 @if ($order->payment_method == 'wire')
                     <div>
-                        <p>Dades per a la transferencia:</p>
+                        <h5 class="mb-4">Dades per a la transferència:</h5>
                         <ul>
-                            <li>Número de la compte: ES-XXXXXXXXXXXXX</li>
-                            <li>Concepte de la transferència: Comanda:#{{ $order->reference }}</li>
+                            <li class="mb-2">Número de la compte: <br> ES-XXXXXXXXXXXXX</li>
+                            <li>Concepte de la transferència: <br> Comanda:#{{ $order->reference }}</li>
                         </ul>
                     </div>
                 @endif
-                @include('public.order_pdf', ['order' => $order])
+                {{-- @include('public.order_pdf', ['order' => $order]) --}}
+                <div class="w-max flex flex-col justify-center space-y-4">
+                    <div class="ticket md:w-[500px] bg-[#f9f9f9] p-5 space-y-4">
+                        <div class="items-baseline pb-1">
+                            <h2 class="float-left">{{ __('checkout.ticket') }}</h2>
+                            <img src="{{ $base64 }}" alt="Èter Edicions" style="width: 3em; height: 3em" class="float-right">
+                        </div>
+                        <br class="clear-both">
+                        <hr>
+                        <div class="order-details">
+                            <p><strong>{{ __('form.name') }}:</strong></p><p class="mb-1"> {{ $order->first_name . ' ' . $order->last_name }}</p>
+                            <p><strong>{{ __('form.shipping-address') }}:</strong></p><p class="mb-1">{{ $order->address }}, @if($order->apartment) {{$order->apartment}},@endif <br>{{$order->locality}}, {{$order->zip_code}}</p>
+                            <p><strong>{{ __('form.date') }}:</strong></p><p class="mb-1"> {{ $order->date }}</p>
+                            <p><strong>{{ __('form.reference-number') }}:</strong></p><p class="mb-1"> {{ $order->reference }}</p>
+                            <p><strong>{{ __('form.payment-method') }}:</strong></p><p class="mb-1"> {{ $order->payment_method }}</p>
+                            <!-- Other order details -->
+                        </div>
+
+                        <table class="order-items">
+                            <thead>
+                                <tr>
+                                    <th class="text-start md:pe-2">{{ __('shopping-cart.product') }}</th>
+                                    <th class="text-end px-1 md:px-2">#</th>
+                                    <th class="text-end px-3 md:px-6">{{ __('shopping-cart.price') }}</th>
+                                    <th class="text-end px-3 md:ps-6">{{ __('shopping-cart.total') }}</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($order->details()->get() as $detail)
+                                    <tr>
+                                        <td class="">{{ $detail->book->title }}</td>
+                                        <td class="">{{ $detail->quantity }}</td>
+                                        <td class="">{{ $detail->price_each }}€</td>
+                                        <td class=""><strong>{{ number_format($detail->quantity * $detail->price_each, 2) }}€</strong></td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+
+                        <div>
+                            <p><strong>{{ __('shopping-cart.subtotal') }}:</strong></p><p class="mb-1"> {{ number_format($order->total - $order->shipment_taxes) }}€</p>
+                            <p><strong>{{ __('form.shipping-cost') }}:</strong></p><p class="mb-1"> {{ $order->shipment_taxes }}€</p>
+                            <p><strong>{{ __('shopping-cart.total') }}:</strong></p><p class="mb-1"> {{ $order->total }}€</p>
+                        </div>
+                    </div>
+                    <div class="footer">
+                        {{ __('checkout.thanks-for-your-purchase') }}
+                    </div>
+                </div>
             </div>
         </div>
         <div class="w-1/3 float-right pl-3">
@@ -26,3 +80,4 @@ $locale = 'ca';
     </div>
     <div class="clear-both"></div>
 </x-layouts.app>
+{{-- <link rel="stylesheet" href="{{ asset('css/public/order_pdf.css') }}"> --}}
