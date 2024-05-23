42a8e263aaa54731f1f6af0eefd77396a7b03a07
Raul <rauldominguezsalgado@gmail.com>
2024-05-23 05:10:55 +0200

    Cambios antes del merge con main

--- resources/views/components/layouts/admin/app.blade.php
+++ resources/views/components/layouts/admin/app.blade.php
@@ -21,7 +21,7 @@
     @vite(['resources/js/app.js'])
 </head>
 <body class="flex flex-col md:flex-row space-y-5 md:space-y-5 m-0 bg-surfacelight">
-    {{-- <x-layouts.admin.navigate/> --}}
+    <x-layouts.admin.navigate/>
     <main class="w-full p-5 md:p-8 lg:p-20 space-y-8">
         <x-layouts.admin.usernav/>
         {{$slot}}
