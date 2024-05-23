7d68b4869a89a965296949efd191f1008810a76c
jordirissech <149579009+jordirissech@users.noreply.github.com>
2024-05-23 19:56:10 +0200

    Contact responsive

--- resources/views/public/contact.blade.php
+++ resources/views/public/contact.blade.php
@@ -6,8 +6,8 @@
 
     <link rel="stylesheet" href="{{ asset('css/public/static.css') }}">
 
-    <main class="body flex flex-col justify-between items-center space-y-8 mb-12">
-        <div class="space-y-4 content w-2/3">
+    <main class="flex flex-col justify-between items-center space-y-8 md:px-8 mb-12">
+        <div class="w-full max-w-6xl space-y-4">
             <div id="title">
                 <h2>{{ $page['contents']['h1'] }}</h2>
             </div>
@@ -16,21 +16,21 @@
             </div>
         </div>
         {{-- <hr> --}}
-        <div class="content w-2/3">
+        <div class="w-full max-w-6xl">
             <h4>{{$page['contents']['form-title']}}</h4>
-            <form action="" method="POST" class="w-full flex flex-col items-center space-y-4">
+            <form action="" method="POST" class="w-full flex flex-col items-center">
                 @csrf
-                <div class="w-full flex justify-between space-x-4">
-                    <div class="flex flex-col space-y-2 grow">
+                <div class="w-full md:flex md:justify-between md:space-x-4">
+                    <div class="flex flex-col space-y-2 grow mb-4">
                         <label for="form-name">{{$page['contents']['form-name']}}</label>
                         <input type="text" name="name" id="form-name">
                     </div>
-                    <div class="flex flex-col space-y-2 grow">
+                    <div class="flex flex-col space-y-2 grow mb-4">
                         <label for="form-email">{{$page['contents']['form-email']}}</label>
                         <input type="text" name="email" id="form-email">
                     </div>
                 </div>
-                <div class="w-full flex flex-col space-y-2 ">
+                <div class="w-full flex flex-col mb-4">
                     <label for="form-subject">{{$page['contents']['form-subject']}}</label>
                     <select name="subject" id="form-subject">
                         @foreach (json_decode($page['contents']['form-subjects']) as $subject)
@@ -39,7 +39,7 @@
                         @endforeach
                     </select>
                 </div>
-                <div class="w-full flex flex-col space-y-2 ">
+                <div class="w-full flex flex-col mb-4">
                     <label for="form-message">{{$page['contents']['form-message']}}</label>
                     <textarea name="message" cols="30" rows="8" id="form-message"></textarea>
                 </div>
